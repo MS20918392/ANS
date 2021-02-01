30b3bb668f1b3133450d3eab4a93ba7d71d0a462
MS20918392 <78263601+MS20918392@users.noreply.github.com>
2021-02-01 17:08:09 +0530

    Task 2 completed

---
+++ SocketProgramming/Task 2/Client2.py
@@ -0,0 +1,13 @@
+import socket   
+from Configuration import *     
+
+clientSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)       
+clientSocket.connect((ServerIp, Serverport))
+letter = input("Enter Number :") 
+clientSocket.sendall(letter.encode()) 
+if(letter == '0'):
+    clientSocket.close()
+else:
+    print (clientSocket.recv(1024).decode(),"\n") 
+
+         
